696a9a45840d81c28964cf3ed22ffccd890bf820
yos-sk <stl1frs2424@gmail.com>
2021-10-21 16:15:44 +0900

    Update run.py

--- nanoEM/run.py
+++ nanoEM/run.py
@@ -1,25 +1,21 @@
 #! /usr/bin/env python
 
 import os, subprocess, shutil
-from .convert_ref import *
-from .convert_reads import *
-from .best_align import *
-from .call_methylation import *
+from convert_ref import *
+from convert_reads import *
+from best_align import *
+from call_methylation import *
 
 def convert_ref_main(args):
-
-    if args.ref[-3:] == '.fa':
-        hout = args.ref[:-3] + '_converted.fa.gz'
-    elif args.ref[-6:] == '.fa.gz':
-        hout = hout = args.ref[:-6] + '_converted.fa.gz'    
-    conversion_ref(args.ref, hout)
+    
+    conversion_ref(args.ref, args.out_prefix)
 
 
 def best_align_main(args):
     
-    conversion_reads(args.fastq)
+    #conversion_reads(args.fastq)
 
-    prefix = args.fastq.split('.fa')[0][:-3]
+    prefix = args.fastq.split('.fq')[0]
 
     if shutil.which("minimap2") is None:
         print("Minimap2 does not exist", file=sys.stderr)
@@ -29,15 +25,19 @@ def best_align_main(args):
         print("Samtools does not exist", file=sys.stderr)
         sys.exit(1)
     
-    command = 'minimap2 -t ' + args.thread + ' --split-prefix temp_sam1 -ax map-ont ' + args.ref + '_converted.fa.gz ' + \
-                             prefix + '_CT.fa  --eqx | samtools view -b | samtools sort -@ ' + args.thread + ' -o 1.sorted.bam'
+    if not os.path.exists(args.out_prefix):
+        print("Output directory does not exit!", file=sys.stderr)
+        sys.exit(1)
+
+    command = 'minimap2 -t ' + str(args.thread) + ' --split-prefix temp_sam1 -ax map-ont ' + args.ref + ' ' + \
+                             prefix + '_CT.fq.gz  --eqx | samtools view -b | samtools sort -@ ' + str(args.thread) + ' -o ' + args.out_prefix + '1.sorted.bam'
     subprocess.check_call(command, shell=True)
 
     command = 'samtools index 1.sorted.bam'
     subprocess.check_call(command, shell=True)
 
-    command = 'minimap2 -t ' + args.thread + ' --split-prefix temp_sam1 -ax map-ont ' + args.ref + '_converted.fa.gz ' + \
-                             prefix + '_GA.fa --eqx | samtools view -b | samtools sort -@ ' + args.thread + ' -o 2.sorted.bam'
+    command = 'minimap2 -t ' + str(args.thread) + ' --split-prefix temp_sam1 -ax map-ont ' + args.ref + ' ' + \
+                             prefix + '_GA.fq.gz --eqx | samtools view -b | samtools sort -@ ' + str(args.thread) + ' -o ' + args.out_prefix + '2.sorted.bam'
     subprocess.check_call(command, shell=True)
 
     command = 'samtools index 2.sorted.bam'
@@ -45,10 +45,10 @@ def best_align_main(args):
 
     out = dict()
 
-    extract_information("1.sorted.bam", 1, out)
-    extract_information("2.sorted.bam", 2, out)
+    extract_information(args.out_prefix + "1.sorted.bam", 1, out)
+    extract_information(args.out_prefix + "2.sorted.bam", 2, out)
 
-    compare(out, "1.sorted.bam", args.fastq)
+    compare(out, args.out_prefix + "1.sorted.bam", args.fastq, args.out_prefix)
 
 
 def call_mathylation_main(args):
@@ -58,20 +58,34 @@ def call_mathylation_main(args):
     
 
     if args.ref:
-        command = 'sambamba mpileup output_CT.sorted.bam -L ' + args.cpgs + ' -o ' +  args.out_prefix + 'pileup_CT.tsv -t ' + \
-                   args.thread + ' --samtools -f ' + args.ref_fasta
-        subprocess.check_call(command, shell=True)
+        #command = 'sambamba mpileup output_CT.sorted.bam -L ' + args.cpgs + ' -o ' +  args.out_prefix + 'pileup_CT.tsv -t ' + \
+        #           args.thread + ' --samtools -f ' + args.ref_fasta
+        #subprocess.check_call(command, shell=True)
 
-        command = 'sambamba mpileup output_GA.sorted.bam -L ' + args.cpgs + ' -o ' +  args.out_prefix + 'pileup_GA.tsv -t ' + \
-                   args.thread + ' --samtools -f ' + args.ref_fasta
-        subprocess.check_call(command, shell=True)
+        hout = args.out_prefix + 'pileup_CT.tsv'
+        subprocess.check_call(['samtools', 'mpileup', '-l', args.cpgs, '-f', args.ref_fasta, 'output_CT.sorted.bam'], stdout = hout)
+        hout.close()
+
+        #command = 'sambamba mpileup output_GA.sorted.bam -L ' + args.cpgs + ' -o ' +  args.out_prefix + 'pileup_GA.tsv -t ' + \
+        #           args.thread + ' --samtools -f ' + args.ref_fasta
+        #subprocess.check_call(command, shell=True)
+        hout = args.out_prefix + 'pileup_GA.tsv'
+        subprocess.check_call(['samtools', 'mpileup', '-l', args.cpgs, '-f', args.ref_fasta, 'output_GA.sorted.bam'], stdout = hout)
+        hout.close()
     
     else:
-        command = 'sambamba mpileup output_CT.sorted.bam -L ' + args.cpgs + ' -o ' +  args.out_prefix + 'pileup_CT.tsv -t ' + args.thread
-        subprocess.check_call(command, shell=True)
+        #command = 'sambamba mpileup output_CT.sorted.bam -L ' + args.cpgs + ' -o ' +  args.out_prefix + 'pileup_CT.tsv -t ' + args.thread
+        #subprocess.check_call(command, shell=True)
+        hout = args.out_prefix + 'pileup_CT.tsv'
+        subprocess.check_call(['samtools', 'mpileup', '-l', args.cpgs, 'output_CT.sorted.bam'], stdout = hout)
+        hout.close()
 
-        command = 'sambamba mpileup output_GA.sorted.bam -L ' + args.cpgs + ' -o ' +  args.out_prefix + 'pileup_GA.tsv -t ' + args.thread
-        subprocess.check_call(command, shell=True)
+        #command = 'sambamba mpileup output_GA.sorted.bam -L ' + args.cpgs + ' -o ' +  args.out_prefix + 'pileup_GA.tsv -t ' + args.thread
+        #subprocess.check_call(command, shell=True)
+
+        hout = args.out_prefix + 'pileup_GA.tsv'
+        subprocess.check_call(['samtools', 'mpileup', '-l', args.cpgs, 'output_GA.sorted.bam'], stdout = hout)
+        hout.close()
     
     fw = args.out_prefix + 'pileup_CT.tsv'
     re = args.out_prefix + 'pileup_GA.tsv'
